7614726125d10224a23b30f88b9961afe8b3e066
Savith Panamgama <geethikasavith@gmail.com>
2024-08-14 06:36:55 +0530

    alert: fix cors issue

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
 import numpy as np
 import tflite_runtime.interpreter as tflite
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+# Enable CORS
+CORS(app)
 
 # Load TFLite model
 interpreter = tflite.Interpreter(model_path="./model/model.tflite")
@@ -13,31 +17,18 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    return response
-
-
-@app.route('/predict', methods=['POST', 'OPTIONS'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'OPTIONS':
-        # Respond to preflight request
-        return add_cors_headers(make_response('', 204))
-
     data = request.get_json(force=True)
-    input_data = np.array(data, dtype=np.float32)
+    input_data = np.array(data['input'], dtype=np.float32)
 
     interpreter.set_tensor(input_details[0]['index'], input_data)
     interpreter.invoke()
     output_data = interpreter.get_tensor(output_details[0]['index'])
     output_data = np.round(output_data).astype(int)
-    # Change to 7 if 0-7 is desired range
-    output_data = np.clip(output_data, None, 7)
+    output_data = np.clip(output_data, None, 8)
 
-    response = make_response(jsonify(output_data.tolist()))
-    return add_cors_headers(response)
+    return jsonify({'prediction': output_data.tolist()})
 
 
 if __name__ == '__main__':
